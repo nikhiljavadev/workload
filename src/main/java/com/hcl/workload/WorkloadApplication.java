bc8ce37b9989ac09e8b1716749be14a40324feae
Nikhil Masam <nikhil.javadev65@gmail.com>
2025-12-19 21:53:25 +0530

    workload first commit

---
+++ src/main/java/com/hcl/workload/WorkloadApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.workload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadApplication.class, args);
+	}
+
+}
